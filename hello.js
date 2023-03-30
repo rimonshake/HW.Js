0b0a80cd39ddcbdc7b62fa4e68a6857b339ea45d
rimonshake <126230240+rimonshake@users.noreply.github.com>
2023-03-30 11:14:40 -0500

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,91 @@
+//1Variable hosse computer memory ar sei space jekhane bivinno value joma rakha hoi
+// 2Rulse to write variable:
+// var (variableName)= value
+// example:
+var num= 20;
+var name= "Rimon";
+
+// 3 kono variable ar value cotesion("",'') ar moddhe likhle take string type variable bole
+// Example:
+var strType= "Rimon";
+console.log(strType)
+
+// 4 kono variable ar value cotesion("",'') bade likhle take Number type variable bole
+// Example:
+var numType= 20;
+console.log(num)
+
+// 4 kono variable ar value true or false janar jonno je variable use kora hoi take boolean type variable bole
+// Example:
+var boolType= true;
+console.log(boolType)
+
+
+// Rulse to use toUpperCase & toLowerCase Method
+// Example:
+var about="RIMON IS A GOOD BOY";
+console.log(about.toLowerCase())
+
+var about="rimon is a good boy";
+console.log(about.toUpperCase())
+
+// Rulse to use Math.abs ()
+// example:
+var num= -20;
+console.log(Math.abs(num))
+
+// Rulse to use Math.ceil ()
+// example:
+var num= 1.4;
+console.log(Math.ceil(num))
+
+// Rulse to use Math.floor ()
+// example:
+var num= 1.9;
+console.log(Math.floor(num))
+
+// Rulse to use Math.floor ()
+// example:
+var num= 1.4;
+console.log(Math.round(num))
+
+// Rulse to use Math.random ()
+// example:
+var lotary = Math.random();
+console.log(lotary)
+
+var lotary = Math.random()*100;
+console.log(lotary)
+
+// Rulse to use Math.split ()
+// example:
+var FrName="Rimon , Rana , Ratul";
+console.log(FrName.split("Rana"));
+
+console.log(FrName.indexOf("Ratul"));
+
+// Rulse to (+,-,*,/)
+var num1=30;
+var num2=20;
+console.log(num1+num2);
+
+var num1=30;
+var num2=20;
+console.log(num1-num2);
+
+var num1=30;
+var num2=20;
+console.log(num1*num2);
+
+var num1=100;
+var num2=20;
+console.log(num1/num2);
+
+var num1=100;
+var num2=30;
+console.log(num1%num2);
+
+var num1="30.33";
+var num1=parseFloat(num1)
+var num2=20;
+console.log(num1+num2);
